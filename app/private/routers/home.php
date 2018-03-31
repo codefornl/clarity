926cc1328068b0e03d8058a65ed8fa0cbae5eac5
Johan Groenen <johan.groenen+github@gmail.com>
2018-03-31 21:41:22 +0200

    also filter promoted empty

--- app/private/routers/home.php
+++ app/private/routers/home.php
@@ -12,14 +12,17 @@ $app->get('/', function (Request $request, Response $response) {
     $q = $request->getQueryParam("q");
     $result = json_decode($this->client->get('/cbases?q=' . $q)->getBody(), true);
     $cbases = $result["_embedded"]["cbase"];
+    $cbases = array_filter($cbases, function ($cbase) {
+        return !empty($cbase["_embedded"]["usecase"]);
+    });
     $promoted = array_filter($cbases, function ($cbase) {
         return (bool)$cbase["promote"];
     });
-    $nonPromotedNonEmpty = array_filter($cbases, function ($cbase) {
-        return !empty($cbase["_embedded"]["usecase"]) && !$cbase["promote"];
+    $nonPromoted = array_filter($cbases, function ($cbase) {
+        return !$cbase["promote"];
     });
     return $this->view->render($response, 'homepage.html', [
-        'cbases' => array_merge($promoted, $nonPromotedNonEmpty),
+        'cbases' => array_merge($promoted, $nonPromoted),
         'q' => $q,
         'uri' => $request->getUri()
     ]);
